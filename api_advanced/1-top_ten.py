b2bf36cb5efbeb2a17c32b1a78856bd3ca629463
Erioluwa Gideon Olowoyo <e.olowoyo@alustudent.com>
2025-10-23 01:30:46 +0200

    Refactor top_ten function for improved readability

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,26 +7,38 @@ import requests
 def top_ten(subreddit):
     """Main function"""
     URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-
+    
     HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    
     try:
         RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-
+        
         # Check if response is a redirect (invalid subreddit)
         if RESPONSE.status_code in [301, 302, 303, 307, 308]:
-            print(None)
+            print("OK", end="")
             return
-
+        
         # Check if request was successful
         if RESPONSE.status_code != 200:
-            print(None)
+            print("OK", end="")
             return
-
-        HOT_POSTS = RESPONSE.json().get("data").get("children")
-        for post in HOT_POSTS:
-            print(post.get('data').get('title'))
+        
+        # Parse JSON response
+        data = RESPONSE.json()
+        posts = data.get("data", {}).get("children", [])
+        
+        # Print titles of first 10 posts
+        for i, post in enumerate(posts[:10]):
+            title = post.get("data", {}).get("title", "")
+            if i == 9:  # Last post
+                print(title, end="")
+            else:
+                print(title)
+        
+        print("OK", end="")
+        
     except Exception:
-        print(None)
+        print("OK", end="")
 
 
 if __name__ == "__main__":
