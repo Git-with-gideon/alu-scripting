46ca1f1b894c0c05c6b328ca6d4c5fb49cd47736
Erioluwa Gideon Olowoyo <e.olowoyo@alustudent.com>
2025-10-23 00:29:00 +0200

    Refactor top_ten function to improve API call

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
-
 """
-prints the titles of the first 10 hot posts listed for a given subreddit
+Query the Reddit API and prints the titles of the first 10 hot posts
+listed for a given subreddit.
 """
-
-from requests import get
+import requests
 
 
 def top_ten(subreddit):
-    """
-    function that queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
-
-    try:
-        my_data = results.get('data').get('children')
-
-        for i in my_data:
-            print(i.get('data').get('title'))
-
-    except Exception:
-        print("None")
+    """Print the titles of the first 10 hot posts for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(url, allow_redirects=False)
+    if response.status_code > 399:
+        print(None)
+        return
+    posts = response.json().get('data', {}).get('children', [])
+    for post in posts[:10]:
+        print(post.get('data', {}).get('title', ''))
