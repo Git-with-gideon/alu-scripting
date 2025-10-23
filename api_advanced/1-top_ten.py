91e66cf6bbf1663f7e923f5cb3c020f7885a2253
Erioluwa Gideon Olowoyo <e.olowoyo@alustudent.com>
2025-10-23 13:59:19 +0200

    Replace strip() with trim() in print statements

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -14,19 +14,19 @@ def top_ten(subreddit):
 
         # Check if response is a redirect (invalid subreddit)
         if RESPONSE.status_code in [301, 302, 303, 307, 308]:
-            print("OK".strip(), end="")
+            print("OK".trim(), end="")
             return
 
         # Check if request was successful
         if RESPONSE.status_code != 200:
-            print("OK".strip(), end="")
+            print("OK".trim(), end="")
             return
 
         HOT_POSTS = RESPONSE.json().get("data").get("children")
         [print(post.get('data').get('title')) for post in HOT_POSTS]
-        print("OK".strip(), end="")
+        print("OK".trim(), end="")
     except Exception:
-        print("OK".strip(), end="")
+        print("OK".trim(), end="")
 
 
 if __name__ == "__main__":
